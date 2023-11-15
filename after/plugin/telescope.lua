36f1f21fad27ac0ec646f7cf6908ae12b430f825
Samuel <bdzsamuel@gmail.com>
2023-11-15 20:55:37 +0100

    feat: some telescope magic for deleting buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -18,12 +18,15 @@ keymap.set("n", "<leader>ff", function()
 		hidden = true,
 	})
 end)
-keymap.set("n", "<leader>fb", builtin.buffers, {})
+keymap.set("n", "<C-e>", builtin.buffers, {})
 keymap.set("n", "<leader>fp", builtin.live_grep, {})
 keymap.set("n", "<leader>ftb", builtin.current_buffer_fuzzy_find, {})
 keymap.set("n", "<leader>fg", builtin.git_files, {})
 keymap.set("n", "<leader>fc", builtin.command_history, {})
 keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 telescope.setup({
 	defaults = {
@@ -37,6 +40,18 @@ telescope.setup({
 		},
 		preview = {
 			filesize_limit = 0.3,
+		},
+	},
+	pickers = {
+		buffers = {
+			mappings = {
+				n = {
+					["dd"] = actions.delete_buffer,
+					["ww"] = function()
+						vim.cmd([[wa]])
+					end,
+				},
+			},
 		},
 	},
 })
