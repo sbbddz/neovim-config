d73b40641416172cee2872915894a59733b03233
Samuel <bdzsamuel@gmail.com>
2025-02-11 06:20:43 +0100

    add dingllm

--- init.lua
+++ init.lua
@@ -70,14 +70,14 @@ keymap.set("i", "<C-c>", "<ESC>")
 --- [[  PLUGINS  ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -94,6 +94,40 @@ require("lazy").setup({
 			},
 		},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"sbbddz/dingllm.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			-- Credits goes to: https://github.com/yacineMTB/dingllm.nvim
+			local system_prompt =
+				"You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks"
+			local helpful_prompt = "You are a helpful assistant. What I have sent are my notes so far."
+			local dingllm = require("dingllm")
+
+			local function anthropic_help()
+				dingllm.invoke_llm_and_stream_into_editor({
+					url = "https://api.anthropic.com/v1/messages",
+					model = "claude-3-5-sonnet-20241022",
+					api_key_name = "ANTHROPIC_API_KEY",
+					system_prompt = helpful_prompt,
+					replace = false,
+				}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
+			end
+
+			local function anthropic_replace()
+				dingllm.invoke_llm_and_stream_into_editor({
+					url = "https://api.anthropic.com/v1/messages",
+					model = "claude-3-5-sonnet-20241022",
+					api_key_name = "ANTHROPIC_API_KEY",
+					system_prompt = system_prompt,
+					replace = true,
+				}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
+			end
+
+			vim.keymap.set({ "n", "v" }, "<leader>I", anthropic_help, { desc = "llm anthropic_help" })
+			vim.keymap.set({ "n", "v" }, "<leader>i", anthropic_replace, { desc = "llm anthropic" })
+		end,
 	},
 	--- LSP
 	{ "williamboman/mason.nvim" },
@@ -371,6 +405,7 @@ local servers = {
 	"zls",
 	"denols",
 	"hls",
+	"ruby_lsp",
 }
 
 --- [[  LSP CONFIG  ]]
@@ -391,7 +426,7 @@ end
 mason.setup({})
 masonlsp.setup({
 	automatic_installation = false,
-	ensure_installed = servers,
+	ensure_installed = {},
 })
 
 local function lsp_keymaps(bufnr)
@@ -606,8 +641,10 @@ require("conform").setup({
 		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "prettierd", "prettier", stop_after_first = true },
 		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+		ruby = { "rubocop" },
 	},
 })
+
 vim.api.nvim_create_user_command("Format", function(args)
 	local range = nil
 	if args.count ~= -1 then
