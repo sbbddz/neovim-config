415e1690aed454cd9253defba141073ecda1f5dd
Samuel <bdzsamuel@gmail.com>
2025-03-13 17:18:30 +0100

    supermaven + fugitive

--- init.lua
+++ init.lua
@@ -70,14 +70,14 @@ keymap.set("i", "<C-c>", "<ESC>")
 --- [[  PLUGINS  ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -86,6 +86,7 @@ require("lazy").setup({
 	{ "willothy/flatten.nvim", opts = {} },
 	{ "nvim-lua/plenary.nvim" },
 	{ "nvim-telescope/telescope.nvim" },
+	{ "tpope/vim-fugitive" },
 	{
 		"stevearc/oil.nvim",
 		opts = {
@@ -96,37 +97,9 @@ require("lazy").setup({
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	{
-		"sbbddz/dingllm.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		"supermaven-inc/supermaven-nvim",
 		config = function()
-			-- Credits goes to: https://github.com/yacineMTB/dingllm.nvim
-			local system_prompt =
-				"You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks"
-			local helpful_prompt = "You are a helpful assistant. What I have sent are my notes so far."
-			local dingllm = require("dingllm")
-
-			local function anthropic_help()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.anthropic.com/v1/messages",
-					model = "claude-3-5-sonnet-20241022",
-					api_key_name = "ANTHROPIC_API_KEY",
-					system_prompt = helpful_prompt,
-					replace = false,
-				}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
-			end
-
-			local function anthropic_replace()
-				dingllm.invoke_llm_and_stream_into_editor({
-					url = "https://api.anthropic.com/v1/messages",
-					model = "claude-3-5-sonnet-20241022",
-					api_key_name = "ANTHROPIC_API_KEY",
-					system_prompt = system_prompt,
-					replace = true,
-				}, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
-			end
-
-			vim.keymap.set({ "n", "v" }, "<leader>I", anthropic_help, { desc = "llm anthropic_help" })
-			vim.keymap.set({ "n", "v" }, "<leader>i", anthropic_replace, { desc = "llm anthropic" })
+			require("supermaven-nvim").setup({})
 		end,
 	},
 	--- LSP
@@ -339,6 +312,7 @@ cmp.setup({
 		{ name = "buffer", keyword_length = 5 },
 		{ name = "luasnip", keyword_length = 2 },
 		{ name = "jira", keyword_length = 1 },
+		{ name = "supermaven" },
 	},
 	formatting = {
 		format = function(entry, item)
@@ -347,6 +321,7 @@ cmp.setup({
 				nvim_lua = "NVIM",
 				path = "PATH",
 				buffer = "BUFF",
+				supermaven = "AI",
 			}
 
 			local menu_name = short_name[entry.source.name] or entry.source.name
