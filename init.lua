e97b75b1e65c92c356f43a512e5d6fd62339e25e
Samuel <bdzsamuel@gmail.com>
2025-03-22 20:14:11 +0100

    new dap configuration for c#

--- init.lua
+++ init.lua
@@ -360,19 +360,54 @@ dap.configurations.cs = {
 			return vim.fn.input("Path to dll", vim.fn.getcwd(), "file")
 		end,
 	},
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg - process",
+		request = "attach",
+		processId = function()
+			return vim.fn.input("")
+		end,
+	},
 }
 
 keymap.set("n", "<leader>bp", dap.toggle_breakpoint, {})
 keymap.set("n", "<leader>bc", dap.continue, {})
 keymap.set("n", "<leader>br", require("dap.repl").toggle, {})
 keymap.set("n", "<leader>bs", dap.terminate, {})
+local api = vim.api
+local keymap_restore = {}
+dap.listeners.after['event_initialized']['me'] = function()
+  for _, buf in pairs(api.nvim_list_bufs()) do
+    local keymaps = api.nvim_buf_get_keymap(buf, 'n')
+    for _, keymap in pairs(keymaps) do
+      if keymap.lhs == "K" then
+        table.insert(keymap_restore, keymap)
+        api.nvim_buf_del_keymap(buf, 'n', 'K')
+      end
+    end
+  end
+  api.nvim_set_keymap(
+    'n', 'K', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
+end
+
+dap.listeners.after['event_terminated']['me'] = function()
+  for _, keymap in pairs(keymap_restore) do
+    api.nvim_buf_set_keymap(
+      keymap.buffer,
+      keymap.mode,
+      keymap.lhs,
+      keymap.rhs,
+      { silent = keymap.silent == 1 }
+    )
+  end
+  keymap_restore = {}
+end
 --- [[ ]]
 
 --- [[  GLOBALS  ]]
 local servers = {
 	"lua_ls",
 	"ts_ls",
-	"rust_analyzer",
 	"gopls",
 	"templ",
 	"omnisharp",
